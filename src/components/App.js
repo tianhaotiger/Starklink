7f3394f0e306fdfda9c5d3863e2828bf370a0e5a
Tianhao(Tiger) Han <tianhaotiger@163.com>
2021-11-18 00:40:27 -0500

    starlink code

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from './Headers';
+import Footer from './Footer';
+import Main from './Main';
+
+
+
+function App() {
+  return (
+      <div className="App">
+          <Header />
+          <Main />
+          <Footer />
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                ©2021 StarLink Tracker. All Rights Reserved. Website Made by Tianhao Han
+            </div>
+        );
+    }
+}
+
+export default Footer;
